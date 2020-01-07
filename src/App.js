d8bfba9ce18b5b3111c26434a2d429ff65d3f7eb
Bayu Adi Prasetiyo <bayu@leson.id>
2020-01-07 17:01:52 +0700

    init learn react from tania

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Table from './Table'
+import Title from './Title'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+      {
+        name: 'Bergkamp',
+        job: 'Director',
+      },
+    ]
+    return (
+      <div className="container">
+        <Title />
+        <Table characterData={characters}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Title extends Component {
+    render() {
+        return (
+            <h1>Employee Table</h1>
+        )
+    }
+}
+
+export default Title
